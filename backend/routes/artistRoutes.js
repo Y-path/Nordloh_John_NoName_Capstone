b14be20c2173e35ed9bbe96072dd9bddedc07a73
Y-path <jno333@gmail.com>
2024-12-18 18:04:51 -0500

    seventeenth commit

--- backend/routes/artistRoutes.js
+++ backend/routes/artistRoutes.js
@@ -2,15 +2,6 @@ const express = require("express");
 const Artist = require("../models/Artist");
 const router = express.Router();
 
-// Get all artists
-router.get("/", async (req, res) => {
-  try {
-    const artists = await Artist.find();
-    res.status(200).json(artists);
-  } catch (err) {
-    res.status(500).json({ message: "Error fetching artists", error: err });
-  }
-});
 
 // Add a new artist
 router.post("/", async (req, res) => {
